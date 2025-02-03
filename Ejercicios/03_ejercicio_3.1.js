5ff98ed01658f9ba6ec1f7b9a168a9b8e47dbb54
Sara Morales <saramf240614@gmail.com>
2025-02-02 21:32:07 -0300

    Agregué una nueva solución al ejercicio 3.1

--- Ejercicios/03_ejercicio_3.1.js
+++ Ejercicios/03_ejercicio_3.1.js
@@ -21,20 +21,32 @@ const input3 = [
     'py'
 ]
 
+//Primera forma de solucionarlo
+
 const transform = (dataRoute) => {
     let theRoute = dataRoute.join('/').replace(/\/(?!.*\/)/, '.');
 
     return theRoute;
 };
 
-
-
-//cosas que se me han ocurrido: 
-// y si pongo la cadena alreves y quito solo el primer / y luego la vuelvo a voltear?
-//y si hago un join con . en vez de / y luego reemplazo todos los . por / excepto el último
-
-
-
 console.log(transform(input1));
 console.log(transform(input2));
 console.log(transform(input3));
+
+
+// Otra forma de solucionarlo
+
+const transform2 = (dataRoute2) => {
+    let lastElement = dataRoute2.filter((r, index) => index === dataRoute2.length - 1);
+    dataRoute2.pop();
+    let firstString = dataRoute2.join('/');
+    let lastList = [];
+    lastList.push(firstString, lastElement);
+    let theRoute2 = lastList.join('.');
+    
+    return theRoute2;
+};
+
+console.log(transform2(input1));
+console.log(transform2(input2));
+console.log(transform2(input3));
