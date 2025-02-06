ba8d854dba76674485c387b5d1188eabad144c86
Sara Morales <saramf240614@gmail.com>
2025-02-05 23:11:18 -0300

    Resolví el ejercicio 5. Pero

---
+++ Ejercicios/bugAsync.js
@@ -0,0 +1,22 @@
+//Al código le faltaba la promesa y su resolución, devolvía undefined porque no le estabamos
+//indicando que hacer cuando
+
+function obtenerUsuario(id) {
+    let usuario;
+    return new Promise((resolve) => {
+        setTimeout ( () => {
+            if (id === 1) {
+                usuario = { id: 1, nombre: 'Jonh Doe' };
+                resolve(usuario);
+            }
+        }, 2000);
+
+        return usuario;
+    });
+};
+
+obtenerUsuario()
+.then(usuario => obtenerUsuario(usuario));
+
+const usuario = obtenerUsuario(1);
+console.log(usuario);
